19ad3e4816ddda1308fbce5deee101d029f19cbf
Elijah-J <30394324+Elijah-J@users.noreply.github.com>
2021-06-28 13:04:03 -0700

    add db connection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hjxjb.mongodb.net/database0?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.get("/", (req, res) => {
   res.send("API Home");
